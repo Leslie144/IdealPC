5f7040c30cefb7c2a530f0af6b800d8d0871fb93
leslie2952 <85373184+Leslie144@users.noreply.github.com>
2021-10-28 09:22:14 -0500

    update tipo uua

--- src/main/java/pe/edu/upc/serviceinterfaces/ITipouaService.java
+++ src/main/java/pe/edu/upc/serviceinterfaces/ITipouaService.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import pe.edu.upc.entities.Tipoua;
 
-
 public interface ITipouaService {
 	public Integer insert(Tipoua tipoua);
 
 	List<Tipoua> list();
+
+	Tipoua listarId(int idTipoua);
 }
