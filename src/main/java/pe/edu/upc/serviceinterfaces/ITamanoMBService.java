c9a8d97388b13fa302b4582cfd65a41fcff0de89
Romale19 <rodrigomarinleiva@gmail.com>
2021-10-28 11:10:54 -0500

    tmb

--- src/main/java/pe/edu/upc/serviceinterfaces/ITamanoMBService.java
+++ src/main/java/pe/edu/upc/serviceinterfaces/ITamanoMBService.java
@@ -9,4 +9,7 @@ public interface ITamanoMBService {
 	public Integer insert (TamanoMB tamanomb);
 	
 	List<TamanoMB> list();
+	TamanoMB listarId(int idTamanoMB);
+	public void delete(int idTamanoMB);
+
 }
