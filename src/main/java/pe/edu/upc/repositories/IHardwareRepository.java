78d47347e05e35215ecbf3442c573c109a417fd5
Deigo465 <rojaspachecodiegofernando@gmail.com>
2021-10-28 03:31:12 -0500

    HARDWARE + mas decoracion

---
+++ src/main/java/pe/edu/upc/repositories/IHardwareRepository.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.entities.Hardware;
+
+@Repository
+public interface IHardwareRepository extends JpaRepository<Hardware, Integer>{
+	@Query("select count(h.nombreHardware) from Hardware h where h.nombreHardware=:name")
+	public int buscarHardware(@Param("name")String nombre);
+
+}
---
+++ src/main/java/pe/edu/upc/serviceinterfaces/IHardwareService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.serviceinterfaces;
+
+import java.util.List;
+
+import pe.edu.upc.entities.Hardware;
+
+public interface IHardwareService {
+
+	public boolean insert(Hardware hardware);
+	List<Hardware>list();
+	Hardware listarId(int idHardware);
+	public void delete(int idHardware);
+}
