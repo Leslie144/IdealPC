164980fac092fd7a911bf7845b2ff3105ab1c698
Romale19 <rodrigomarinleiva@gmail.com>
2021-11-19 01:19:40 -0500

    pon la fecha

--- src/main/java/pe/edu/upc/repositories/ITipoUsuarioRepository.java
+++ src/main/java/pe/edu/upc/repositories/ITipoUsuarioRepository.java
@@ -1,6 +1,8 @@
 package pe.edu.upc.repositories;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,6 @@ public interface ITipoUsuarioRepository extends JpaRepository<Role, Long>{
 
 	@Query("select count (tu.rol) from Role tu where tu.rol=:nTipousuario")
 	public int TipoUsuarioExistentes(@Param("nTipousuario") String nTipousuario);
+	
+	List<Role> findByRol(String rol);
 }
