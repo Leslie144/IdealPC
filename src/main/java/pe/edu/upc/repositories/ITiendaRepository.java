35b4964f0b2ce166c58bc43ff93a8ea70de0b4d7
leslie2952 <85373184+Leslie144@users.noreply.github.com>
2021-11-22 22:10:18 -0500

    reporte tiendas
    Co-Authored-By: Daniel  <57024259+Shadow-24@users.noreply.github.com>

--- src/main/java/pe/edu/upc/repositories/ITiendaRepository.java
+++ src/main/java/pe/edu/upc/repositories/ITiendaRepository.java
@@ -18,4 +18,11 @@ public interface ITiendaRepository extends JpaRepository<Tienda, Integer> {
 	List<Tienda> findByDistritoNombreDistrito(String nombreDistrito);;
 	
 	List<Tienda> findBynombreTienda(String name);
+	
+	@Query(value="SELECT ti.*, d.nombre_distrito\r\n"
+			+ "from tienda ti \r\n"
+			+ "inner join distrito d on ti.id_distrito = d.id_distrito\r\n"
+			+ "where ti.fecha_registro >= (CURRENT_DATE - 30)",  nativeQuery = true)
+	public List<String[]> quantityStores();
+
 }
